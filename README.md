b6601726c01e37c3a8f6324a48f023cfd60aaaf3
Daniel Schep <daniel@trailsta.sh>
2023-09-11 10:38:06 -0400

    :tada:

---
+++ README.md
@@ -0,0 +1,10 @@
+# OpenStreetMap Americana PWA
+
+This repo contains a Progressive Web Application with [OpenStreetMap
+Americana](https://github.com/ZeLonewolf/openstreetmap-americana/) in order to make it installable
+on cell phones.
+
+It is powered by `@trailastash/stylejs-americana` which wraps the style to conform to the the
+[TrailStash StyleJS spec](https://gitlab.com/trailstash/map-view#trailstash-stylejs-spec)
+
+Check it out here to install it! https://trailastash.github.io/americana-pwa
---
+++ index.js
@@ -0,0 +1,45 @@
+import MapView from "@trailstash/map-view";
+import Americana from "@trailstash/stylejs-americana";
+
+const config = {
+  map: {
+    container: "map",
+    style: new Americana(),
+    zoom: 4,
+    center: [-100, 40],
+    attributionControl: false,
+  },
+  controls: [
+    {
+      position: "bottom-right",
+      type: "GeolocateControl",
+      options: {
+        positionOptions: {
+          enableHighAccuracy: true,
+        },
+        trackUserLocation: true,
+      },
+    },
+    {
+      position: "bottom-right",
+      type: "NavigationControl",
+    },
+    {
+      type: "ScaleControl",
+      options: {
+        unit: "imperial",
+      },
+    },
+    { type: "AutoClosingAttributionControl", position: "top-right" },
+  ],
+  PersistView: true,
+};
+const view = new MapView(config);
+
+// register service worker for PWA
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("./sw.js").then(function (registration) {
+    registration.update();
+    console.log("Service Worker Registered");
+  });
+}
